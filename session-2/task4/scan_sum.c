a234d874b7b5538d7da1c37e6ebacb00a7620914
Chenxuan Li <rndm2830@leeds.ac.uk>
2026-02-20 12:55:12 +0000

    2.20  classwork

--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -4,9 +4,14 @@
 int main( void ) {
 
     // define suitable data
-
+    int number
     // use scanf to read from the terminal
-    // print the output from scanf and the data values 
+    printf("Enter an integer: ");
+    int result = scanf("%d", &number);
 
+    // print the output from scanf and the data values 
+    printf("scanf returned: %d\n", result);
+    printf("You entered: %d\n", number);
+    
     return 0;
 }
--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Chenxuan Li
+Student ID: 201859038
 */
 
 #include <stdio.h>
@@ -10,9 +10,19 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
-
+    char buffer[1000] = "";
+    
     // process the command-line data using appropriate string functions
-
+    
+    strcpy(buffer, argv[1]);
+    // copy the first argv to "buffer"
+    
+    for (int i = 2; i < argc; i++) {
+        strcat(buffer, "-");
+        strcat(buffer, argv[i]);
+    }
+    // add "-" and argv[i] to "buffer"
+    
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
     return 0;
